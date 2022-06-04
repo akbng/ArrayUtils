6066946850cad5916895426ddcb0229f96f37e8a
ankanbag101 <bagankan@gmail.com>
2022-06-04 13:04:44 +0530

    add: comments in the string array library

--- string/ArrayString.sol
+++ string/ArrayString.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.8.0;
 
 import "../openzeppelin/SafeCast.sol";
 
+/// @dev Collection of some commonly used utility function for array of type string
 library ArrayString {
     using SafeCast for int256;
     using SafeCast for uint256;
 
+    /**
+     * @dev Returns if the given value is present in the array or not.
+     * Function complexity is O(n).
+     * It might cost very high gas for larger arrays.
+     *
+     * Requirements:
+     * - input array must not be empty
+     */
     function includes(string[] storage _array, string memory _value)
         internal
         view
@@ -19,6 +28,14 @@ library ArrayString {
         return false;
     }
 
+    /**
+     * @dev Returns the index of the given value in the array.
+     * Function complexity is O(n).
+     * It might cost very high gas for larger arrays.
+     *
+     * Requirements:
+     * - input array must not be empty
+     */
     function indexOf(string[] storage _array, string memory _value)
         internal
         view
@@ -31,6 +48,14 @@ library ArrayString {
         return -1;
     }
 
+    /**
+     * @dev Returns the last found index of the given value in the array.
+     * Function complexity is O(n).
+     * It might cost very high gas for larger arrays.
+     *
+     * Requirements:
+     * - input array must not be empty
+     */
     function lastIndexOf(string[] storage _array, string memory _value)
         internal
         view
@@ -43,6 +68,15 @@ library ArrayString {
         }
     }
 
+    /**
+     * @dev Returns the element present at the given index of the array.
+     * It supports negative indexing where -1 means the last element and -length = first element.
+     * Function complexity is O(1).
+     *
+     * Requirements:
+     * - input array must not be empty
+     * - index must not be greater than the array length
+     */
     function at(string[] storage _array, int256 _index)
         internal
         view
@@ -59,7 +93,16 @@ library ArrayString {
         return _array[index];
     }
 
-    // mutates the original array
+    /**
+     * @dev Remove an element from a given index of the array.
+     * It changes the order of the array.
+     * It mutates the original array.
+     * Function complexity is O(1).
+     *
+     * Requirements:
+     * - input array must not be empty
+     * - index must not be greater than the array length
+     */
     function remove(string[] storage _array, uint256 _index) internal {
         require(_array.length > 0, "ArrayString: array should not be empty");
         require(
@@ -70,6 +113,10 @@ library ArrayString {
         _array.pop();
     }
 
+    /**
+     * @dev Function to check equality of two strings.
+     * This function must not be called from outside.
+     */
     function equals(string memory a, string memory b)
         private
         pure
